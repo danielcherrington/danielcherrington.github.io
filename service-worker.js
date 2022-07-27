060de78b3ab32e1e8da6c247fd5ed4c894437c18
danielcherrington <daniel.cherrington@gmail.com>
2022-07-27 23:17:27 +0000

    Deploying to gh-pages from  @ a9d2eab24509b59220aea2a5c9b0134a10778ef3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.15bac1be3801a3f631bba0e972d7f4bd.js"
+  "/precache-manifest.ef48dce3449c7ee81fd2e70c54668dfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
